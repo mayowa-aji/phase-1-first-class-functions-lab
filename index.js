acc6a65746247901b1cee44096bcafff9c721fcc
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-01-11 16:43:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0,2)
+
+}
+// console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
+
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2)
+
+}
+// console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
+
+// - create an array of two callback functions
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers]
+
+
+function createFareMultiplier(num) {
+  return (fare) => {
+    return num * fare
+  }
+}
+// better way to write the above
+// const fare10Multiplier = createFareMultiplier(10)
+// console.log(createFareMultiplier(10.0)(4))
+
+const fareDoubler = createFareMultiplier(2)
+// console.log(fareDoubler(10.0))
+const fareTripler = createFareMultiplier(3)
+// console.log(fareTripler(10.0))
+
+const selectDifferentDrivers = function(drivers, func) {
+  return func(drivers)
+}
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers))
